e57454d9d657aba570dbec44bf4c969f2a48d30c
chaenabi <find1086@gmail.com>
2022-07-31 17:57:01 +0900

    Initial Commit

---
+++ src/test/java/bluebird/emm/apiclient/ApiclientApplicationTests.java
@@ -0,0 +1,13 @@
+package bluebird.emm.apiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
